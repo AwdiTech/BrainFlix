9c0281b6314e668704ae8f7cc3e1aa2b5a42475a
AwdiTech <raby.awdi@gmail.com>
2024-01-21 13:14:48 -0500

    Implement video upload and handlers

--- src/Pages/Upload/Upload.js
+++ src/Pages/Upload/Upload.js
@@ -1,7 +1,10 @@
 import './Upload.scss';
-import thumbnailPicture from './../../assets/images/Upload-video-preview.jpg';
+import thumbnailDefault from './../../assets/images/Upload-video-preview.jpg';
 import publishIcon from './../../assets/Icons/publish.svg';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 /**
  * Upload Component
@@ -25,13 +28,67 @@ import { useNavigate } from 'react-router-dom';
 
 function Upload() {
 
+    const [thumbnailSrc, setThumbnailSrc] = useState(thumbnailDefault);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [newVideoTitle, setNewVideoTitle] = useState('');
+    const [newVideoDesc, setNewVideoDesc] = useState('');
+
+
     let navigate = useNavigate();
+
 
     const uploadButtonHandler = function (event) {
         event.preventDefault();
-        alert("Video Successfully Uploaded!");
-        navigate('/');
+
+        if (newVideoTitle && newVideoDesc) {
+
+            const newVideoID = uuidv4();
+
+            const newVideo = {
+                "id": newVideoID,
+                "title": newVideoTitle,
+                "channel": "UserName123_Channel",
+                "image": ''
+            }
+            const newVideoDetails = {
+                "id": newVideoID,
+                "title": newVideoTitle,
+                "channel": "UserName123_Channel",
+                "image": '',
+                "description": newVideoDesc,
+                "views": "0",
+                "likes": "0",
+                "duration": "0:00",
+                "video": "",
+                "timestamp": new Date(),
+                "comments": []
+            }
+
+            const formData = new FormData();
+            formData.append("newVideo", JSON.stringify(newVideo));
+            formData.append("newVideoDetails", JSON.stringify(newVideoDetails));
+            formData.append('image', selectedFile);
+
+            axios.post('http://localhost:8080/videos', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+                .then(response => {
+                    console.log("Success response: ", response)
+                    alert("Video Successfully Uploaded!");
+                    navigate('/');
+                })
+                .catch((err) => {
+                    console.log(err)
+                });
+
+        }
+        else {
+            alert("Error: Title and Description required.");
+        }
     }
+
 
     const cancelButtonHandler = function (event) {
         event.preventDefault();
@@ -39,8 +96,24 @@ function Upload() {
         navigate('/');
     }
 
+
+    const handleImageChange = (event) => {
+        if (event.target.files && event.target.files[0]) {
+            const imageFile = event.target.files[0];
+            setSelectedFile(imageFile)
+
+            let reader = new FileReader();
+
+            reader.onload = () => {
+                setThumbnailSrc(reader.result);
+            }
+            reader.readAsDataURL(imageFile);
+        }
+    }
+
+
     return (
-        <form className='upload-form'>
+        <form className='upload-form' encType="multipart/form-data">
             <header className='upload__header'>
                 <h1 className='upload__header-title'>Upload Video</h1>
             </header>
@@ -48,27 +121,47 @@ function Upload() {
             <section className='upload-details'>
                 <article className='thumbnail'>
                     <p className='thumbnail__label special-label'>VIDEO THUMBNAIL</p>
-                    <img className='thumbnail__image' src={thumbnailPicture} alt='Upload Image Thumbnail' />
+                    <img className='thumbnail__image' src={thumbnailSrc} alt='Upload Thumbnail' />
+                    <input
+                        type='file'
+                        accept='image/*'
+                        onChange={handleImageChange}
+                        className='thumbnail__input'
+                    />
                 </article>
 
                 <article className='input-video-details'>
                     <label className='input-video-details__title-label special-label' htmlFor="video-title">TITLE YOUR VIDEO</label>
-                    <input className='input-video-details__title-field input-field' type='text' name='video-title' placeholder='Add a title to your video' autofocus />
+                    <input
+                        className='input-video-details__title-field input-field'
+                        type='text' name='video-title'
+                        placeholder='Add a title to your video'
+                        autoFocus
+                        value={newVideoTitle}
+                        onChange={(event) => setNewVideoTitle(event.target.value)}
+                    />
                     <label className='input-video-details__description-label special-label' htmlFor="video-description">ADD A VIDEO DESCRIPTION</label>
-                    <textarea className='input-video-details__description-field input-field' name="video-description" placeholder='Add a description to your video' rows='4'/>
+                    <textarea
+                        className='input-video-details__description-field input-field'
+                        name="video-description"
+                        placeholder='Add a description to your video'
+                        rows='4'
+                        value={newVideoDesc}
+                        onChange={(event) => setNewVideoDesc(event.target.value)}
+                    />
                 </article>
             </section>
 
             <section className='upload-options'>
                 <button className='upload-options__upload-button cta-button' onClick={uploadButtonHandler}>
-                <img src={publishIcon} alt="" className='publish-icon style-icon' />
-                PUBLISH
+                    <img src={publishIcon} alt="" className='publish-icon style-icon' />
+                    PUBLISH
                 </button>
-                <button className='upload-options__cancel-button cta-button' id='cancel-button' onClick={cancelButtonHandler}>CANCEL</button>
+                <button type='submit' className='upload-options__cancel-button cta-button' id='cancel-button' onClick={cancelButtonHandler}>CANCEL</button>
             </section>
         </form>
 
     );
-}   
+}
 
 export default Upload;
